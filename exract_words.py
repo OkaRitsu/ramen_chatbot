861c62a1ead39eb96eeca6a20d8370b889a22e94
er1152 <84836565+er1152@users.noreply.github.com>
2021-10-05 00:51:50 +0900

    読み込むコーパスを変更

--- exract_words.py
+++ exract_words.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import MeCab
 
-# tagger = MeCab.Tagger(" -r /etc/mecabrc -d /usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd")
+#tagger = MeCab.Tagger(" -r /etc/mecabrc -d /usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd")
 tagger = MeCab.Tagger("-Owakati -d /opt/homebrew/lib/mecab/dic/mecab-ipadic-neologd")
 
 
@@ -22,11 +22,13 @@ def tokenize(content, lower):
 
 
 def search_corpus(user_word):
+    print(f"ユーザーワード:{user_word}")
     user_word = tokenize(user_word, True) #形態素解析
-    print(user_word)
-    with open("./data/ramen_corpus.txt", "r") as f: 
-        corpus = f.read().replace("'",'').replace('[','').replace(']','').replace(' ','').replace('\n','').split(",")
-    user_key_words = [word for word in user_word if word in corpus]
+    print(f"形態素解析結果：{user_word}")
+
+    with open("./data/word2vec.txt", "r") as f:
+        corpus = f.read().split("\n")
+    user_key_words = [word for word in user_word if word in corpus] 
 
     # print(f"コーパスの単語数：{len(corpus)}")
     return user_key_words
